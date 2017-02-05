53ab92883469d443848ae796adfb9a8e3286731c
dev-wangliugen <253192463@qq.com>
2017-02-05 21:25:51 +0800

    YLRecordControlViewUI

--- YLVideoRecord/YLVideoRecord/YLRecordControlView.swift
+++ YLVideoRecord/YLVideoRecord/YLRecordControlView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 open class YLRecordControlView: UIView {
-    open var progressView: YLProgressView!
+
 
     
     /// 录制的控制代理
@@ -18,7 +18,8 @@ open class YLRecordControlView: UIView {
     fileprivate var recordButton = UIButton()
     fileprivate var stopOrChioceButton = UIButton()
     fileprivate var cancalButton = UIButton()
-    
+    //持有进度条
+    open var progressView: YLProgressView!
     ///
     ///
     /// - Parameter frame: 控制视图的size建议最少大于120
@@ -32,12 +33,21 @@ open class YLRecordControlView: UIView {
     }
     
     func initialUI() {
-        let progressView = YLProgressView(frame: CGRect(x: 0, y: 100, width: frame.size.width, height: 20))
+        progressView = YLProgressView(frame: CGRect(x: 0, y: 100, width: frame.size.width, height: 20))
         self.addSubview(progressView)
         
         recordButton.frame = CGRect(x: frame.width/2 - 30, y: (frame.height - 20)/2 - 30, width: 60, height: 60)
         self.addSubview(recordButton)
         recordButton.addTarget(self, action: #selector(recordButtonAction(sender:)), for: .touchUpInside)
+        
+        stopOrChioceButton.frame = CGRect(x: frame.width - 60, y: (frame.height - 20)/2 - 30, width: 50, height: 40)
+        self.addSubview(stopOrChioceButton)
+        stopOrChioceButton.addTarget(self, action: #selector(stopOrChioceButtonAction(sender:)), for: .touchUpInside)
+        
+        cancalButton.frame = CGRect(x: 10, y: (frame.height - 20)/2 - 30, width: 50, height: 40)
+        self.addSubview(cancalButton)
+        cancalButton.addTarget(self, action: #selector(cancalButtonAction(sender:)), for: .touchUpInside)
+
         
         progressView.startProgress(progress: 1, totalTimer: 10)
         
@@ -47,6 +57,14 @@ open class YLRecordControlView: UIView {
     func recordButtonAction(sender:UIButton) {
 //        let status = sender.isSelected
 //        sender.isSelected = !sender.isSelected
+        
+    }
+    
+    func stopOrChioceButtonAction(sender:UIButton) {
+        
+    }
+    
+    func cancalButtonAction(sender: UIButton) {
         
     }
 
